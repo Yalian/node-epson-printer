e533dcb9fc9021f45b9e6a2ac9cff6b9d33ff308
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-08-26 10:41:35 +0700

    Print custom escpos to hoi-escpos

---
+++ src/hoi-escpos/index.js
@@ -0,0 +1,13 @@
+'use strict';
+/**
+ * Adapters
+ */
+exports.Network = require('./adapter/network');
+exports.Console = require('./adapter/console');
+/**
+ * Printer Supports
+ */
+exports.Image   = require('./image');
+exports.Printer = require('./printer');
+exports.Adapter = require('./adapter');
+exports.command = require('./commands');
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const escpos = require('escpos');
+const escpos = require('./hoi-escpos');
 
 // Select the adapter based on your printer type
 // const device  = new escpos.USB();
-const device  = new escpos.Network('192.168.1.14');
+const device  = new escpos.Network('192.168.9.91');
 // const device  = new escpos.Serial('/dev/usb/lp0');
 
 const printer = new escpos.Printer(device);
@@ -24,45 +24,45 @@ const printer = new escpos.Printer(device);
 //
 // });
 
-// escpos.Image.load(__dirname + '/tux.png', function(image){
-//   device.open(function(){
-//     printer
-//       .align('ct')
-//       .setColor(1)
-//       .hoiImage(image) // Use hoitImage for epson tm u220
-//       .cut(false, 5)
-//       .close()
-//
-//   });
-// });
-
-let receiptImagePro = new Promise(resolve => {
-  escpos.Image.load(__dirname + '/receipt5.png', function(image){
-    resolve(image)
-  });
-})
-
-let tuxImagePro = new Promise(resolve => {
-  escpos.Image.load(__dirname + '/tux.png', function(image){
-    resolve(image)
-  });
-})
-
-Promise.all([receiptImagePro, tuxImagePro]).then(([receiptImage, tuxImage]) => {
+escpos.Image.load(__dirname + '/sample.png', function(image){
   device.open(function(){
-
     printer
       .align('ct')
-      .setColor(0)
-      .text("text: set color black")
-      .text("print receipt image")
-      .hoiImage(receiptImage)
-      .setColor(1)
-      .text("text: set color black")
-      .text("print tux image")
-      .hoiImage(tuxImage)
-      .cut(false, 5)
+      // .setColor(1)
+      .hoiImage(image) // Use hoitImage for epson tm u220
+      .cut(false, 10)
       .close()
 
   });
 });
+
+// let receiptImagePro = new Promise(resolve => {
+//   escpos.Image.load(__dirname + '/receipt5.png', function(image){
+//     resolve(image)
+//   });
+// })
+
+// let tuxImagePro = new Promise(resolve => {
+//   escpos.Image.load(__dirname + '/tux.png', function(image){
+//     resolve(image)
+//   });
+// })
+
+// Promise.all([receiptImagePro, tuxImagePro]).then(([receiptImage, tuxImage]) => {
+//   device.open(function(){
+
+//     printer
+//       .align('ct')
+//       .setColor(0)
+//       .text("text: set color black")
+//       .text("print receipt image")
+//       .hoiImage(receiptImage)
+//       .setColor(1)
+//       .text("text: set color black")
+//       .text("print tux image")
+//       .hoiImage(tuxImage)
+//       .cut(false, 5)
+//       .close()
+
+//   });
+// });
